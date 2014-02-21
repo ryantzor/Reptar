e23beb629f6920d8c5692cba33c3601bb51d6ac9
ryantzor <ryantulleris@gmail.com>
2014-02-21 00:55:34 -0500

    Added dialog for load previous button
    the dialog has 2 buttons: cancel and yes. The "Yes" button currently
    takes you to the activity where we will have our game or statistic
    information.

--- src/com/example/reptar/MainActivity.java
+++ src/com/example/reptar/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.reptar;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,62 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    
+        
+        
+        
+    	Button loadPreviousButton = (Button) findViewById(R.id.btnLoadPreviousSim);
+    	loadPreviousButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+			    builder.setMessage(R.string.are_you_sure);
+			    builder.setTitle(R.string.warning);
+			    
+			    builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+					
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// start the simulation activity
+						Intent intent = new Intent(MainActivity.this, Simulation.class);
+						startActivity(intent);
+					}
+				});
+			    
+			    builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+					
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						// do nothing 
+					}
+				});
+			    
+			    AlertDialog dialog = builder.create();
+			    dialog.show();
+			}
+		});
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
 
@@ -20,4 +81,16 @@ public class MainActivity extends Activity {
         return true;
     }
     
+
+    
+    			    
+    			  	
 }
+    
+
+    
+    
+
+
+
+
