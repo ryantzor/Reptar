340bc1b4a6c00185d97a43cff5d1877f23ef4c23
Kyle Berry <kyle.a.berry@gmail.com>
2014-03-04 17:38:17 -0500

    Add date

--- src/com/example/reptar/System.java
+++ src/com/example/reptar/System.java
@@ -2,11 +2,10 @@ package com.example.reptar;
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Date;
 
 public class System {
-	
-	
-	
+	Date Time;
 	Comparator<Event> comparator; //= new somthing;
 	PriorityQueue<Event> queue; //= new PriorityQueue<Event>(10, comparator);
 	
