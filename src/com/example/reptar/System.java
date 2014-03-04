f03f2c87a4def41e0e533a28ed481f41cbb71651
Kyle Berry <kyle.a.berry@gmail.com>
2014-03-04 16:35:33 -0500

    Add something

--- src/com/example/reptar/System.java
+++ src/com/example/reptar/System.java
@@ -4,7 +4,10 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class System {
-	//Comparator<Event> comparator = new somthing;
-	//PriorityQueue<Event> queue = new PriorityQueue<Event>(10, comparator);
+	Comparator<Event> comparator; //= new somthing;
+	PriorityQueue<Event> queue; //= new PriorityQueue<Event>(10, comparator);
 	
+	void System() {
+		
+	}
 }
