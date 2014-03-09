37e4e96afbada26982e0738de524cbd3224ddb23
ryantzor <ryantulleris@gmail.com>
2014-03-08 20:33:08 -0500

    Update Plants.java
    github error fix

--- src/com/example/reptar/Plants.java
+++ src/com/example/reptar/Plants.java
@@ -7,9 +7,6 @@ public class Plants extends Organism{
 		//Magic numbers
 		this.threatLevel = 0;
 		this.terrainAdaptability = 20;
-<<<<<<< HEAD
-		
-=======
->>>>>>> defa3c038d06277e7f1839445e1bb4e422219a37
+
 	}
 }
