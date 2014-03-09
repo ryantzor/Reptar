5de6b0b96890021bc52ea9f2c6741f52c4b030ea
ryantzor <ryantulleris@gmail.com>
2014-03-08 20:31:17 -0500

    Update TRex.java
    stupid github error fix

--- src/com/example/reptar/TRex.java
+++ src/com/example/reptar/TRex.java
@@ -1,6 +1,5 @@
 package com.example.reptar;
 
-<<<<<<< HEAD
 public class TRex extends Organism 
 	{
 	
@@ -11,23 +10,5 @@ public class TRex extends Organism
 			this.terrainAdaptability = 20.00;
 			this.threatLevel = 90.00;
 	}
-=======
-<<<<<<< HEAD
-public class TRex extends Organism 
-	{
-	
-	public TRex(){
-		
-			super();
-			//Magic numbers
-			this.terrainAdaptability = 20.00;
-			this.threatLevel = 90.00;
-	}
-=======
-public class TRex {
-	
->>>>>>> 5c181da2820770bb2aeff336c93f1afd76d83129
->>>>>>> defa3c038d06277e7f1839445e1bb4e422219a37
-	
 
 }
